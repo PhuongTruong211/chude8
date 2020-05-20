913ffcccd9b69cfc4b47992a160660341d4c9326
Pham Minh Ty <phuongb1609542@student.ctu.edu.vn>
2020-05-20 08:50:09 +0700

    Thêm tập tin createtable.php

---
+++ createtable.php
@@ -0,0 +1,28 @@
+<?php
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+   $db = pg_connect(pg_connection_string_from_database_url() );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+   
+   $sql =<<<EOF
+     
+      CREATE TABLE SINHVIEN
+      (ID INT PRIMARY KEY     NOT NULL,
+      USERNAME           CHAR(50)    NOT NULL,
+      PASSWORD           CHAR(50)     NOT NULL);
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Table created successfully\n";
+   }
+   pg_close($db);
+?>
